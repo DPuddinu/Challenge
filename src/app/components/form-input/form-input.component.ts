691d4ddb6db569c2721c5203c98e45cb083cd4bf
Dario Puddinu <dario.puddinu@abinsula.com>
2024-12-09 18:28:33 +0100

    style: form-input component

--- src/app/components/form-input/form-input.component.ts
+++ src/app/components/form-input/form-input.component.ts
@@ -18,9 +18,29 @@ type InputType = 'text' | 'number' | 'email' | 'password';
   ],
   template: ` @if (control) {
     <div>
-      <input [required]="isRequired()" [type]="type()" [id]="inputId()" [formControl]="control" />
-      <app-validation-errors [customErrorMessages]="customErrorMessages()" [errors]="control.errors">
-      </app-validation-errors>
+      <label
+        for="Username"
+        class="relative block rounded-md border border-gray-200 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
+      >
+        <input
+          [required]="isRequired()"
+          [type]="type()"
+          [id]="inputId()"
+          [formControl]="control"
+          class="py-2 px-4 peer border-none bg-transparent placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0"
+          [placeholder]="label()"
+        />
+
+        <span
+          class="pointer-events-none absolute start-2.5 top-0 -translate-y-1/2 bg-white p-0.5 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs"
+        >
+          {{ label() }} 
+        </span>
+      </label>
+      @if(control.touched && control.dirty) {
+        <app-validation-errors [customErrorMessages]="customErrorMessages()" [errors]="control.errors">
+        </app-validation-errors>
+      }
     </div>
   }`,
   changeDetection: ChangeDetectionStrategy.OnPush
